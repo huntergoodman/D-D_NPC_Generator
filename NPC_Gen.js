120cf534609dc25f390a93c4d108710188037432
huntergoodman <hunterleegoodman@outlook.com>
2019-08-18 21:39:37 -0500

    Add files via upload

---
+++ NPC_Gen.js
@@ -0,0 +1,155 @@
+//NPC stat variables
+var npcNum1 = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+var npcNum2 = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+var npcNum3 = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+var npcNum4 = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+var npcNum5 = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+var npcNum6 = ['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+//Dwarf Names
+var dwarfNameMale = ['Adrik', 'Alberich', 'Baern', 'Barendd', 'Brottor', 'Breunor', 'Dain', 'Darrak', 'Delg', 'Eberk', 'Einkil', 'Fargrim', 'Flint', 'Gardain', 'Harbek', 'Kildrak', 'Morgran', 'Orsik', 'Oskar', 'Rangrim', 'Rurik', 'Taklinn', 'Thoradin', 'Thorin', 'Tordek', 'Traubon', 'Travok', 'Ulfgar', 'Viet', 'Vondal'];
+
+var dwarfNameFemale = ['Amber', 'Artin', 'Audhild', 'Bardryn', 'Dagnal', 'Diesa', 'Eldith', 'Falkrunn', 'Finellen', 'Gunnloda', 'Gurdis', 'Helja', 'Hlin', 'Kathra', 'Kristryd', 'Ilde', 'Liftrasa', 'Mardred', "Riswynn", 'Sannl', 'Torbera', 'Torgga', 'Vistra'];
+
+
+var dwarfClanName = ['Balderk', 'Battlehammer', 'Brawnanvil', 'Dankil', 'Fireforge', 'Frosbeard', 'Gorunn', 'Holderhek', 'Ironfist', 'Loderr', 'Lutgeher', 'Rumnaheim', 'Strakeln', 'Torunn', 'Ungart'];
+
+//Elf Names
+var elfMaleNames = ['Adran', 'Aelar', 'Aramil', 'Arannis', 'Aust', 'Beiro', 'Berrian', 'Carric', 'Enialis', 'Erevan', 'Galinndan', 'Hadarai', 'Heian', 'Himo', 'Immeral', 'Ivellios', 'Laucian', 'Mindartis', 'Paelaias', 'Peren', 'Quarion', 'Riardon', 'Rolen', 'Soveliss', 'Thamior', 'Tharivol', 'Theren', 'Varis'];
+
+var elfFemaleNames = ['Adrie', 'Althaea', 'Anastrianna', 'Andraste', 'Antinua', 'Bethrynna', 'Birel', 'Caelynn', 'Drusilia', 'Enna', 'Felosial', 'Ielenia', 'Jelenneth', 'Keyleth', 'Leshanna', 'Lia', 'Meriele', 'Mialee', 'Naivara', 'Quelenna', 'Quillathe', 'Sariel', 'Shanairra', 'Shava', 'Silaqui', 'Theirastra', 'Thia', 'Vadania', 'Valanthe', 'Xanaphia'];
+
+var elfFamilyNames = ['Amakiir(Gemflower)', 'Amastacia(Starflower)', 'Galanodel(Moonwhisper)', 'Holimion(Diamonddew)', 'Ilphelkiir(Gemblossom)', 'Liadon(Silverfrond)', 'Meliamne(Oakenheel)', 'Nal&#x457o(Nightbreeze)', 'Siannodel(Moonbrook)', 'Xiloscient(Goldpetal)'];
+
+//Halfling Names
+var halflingMaleNames = ['Alton','Ander','Cade','Corrin','Eldon','Errich','Finnan','Garret','Lindal','Lyle','Merric','Milo','Osborn','Perrin','Reed','Roscoe','Wellby'];
+
+var halflingFemaleNames = ['Andry','Bree','Callie','Cora','Euphemia','Jillian','Kithri','Lavinia','Lidda','Merla','Nedda','Paela','Portia','Seraphina','Shaena','Trym','Vani','Verna'];
+
+var halflingFamilyNames = ['Brushgather','Goodbarrel','Greenbottle','High-hill','Hilltopple','Leagallow','Tealeaf','Thorngage','Tosscobble','Underbough'];
+
+//NPC Appearance
+var npcAppearance = ['Distinctive jewlery: earrings, necklace, circlet, bracelets', 'Piercings', 'Flamboyant or outlandish clothes', 'Formal clean clothes', 'Ragged dirty clothes', 'Pronounced scar', 'Missing teeth', 'missing fingers', 'unusual eye color(or two different colors)', 'Tattoos', 'Birthmark', 'Unusual skin color', 'Bals', 'Braided beard or hair', 'Unusual hair color', 'Nervious eye twitch', 'Distinctive nose', 'Distinctive posture(crooked or ridged)', 'Exceptionally beautiful', 'Exeptionally ugly'];
+//NPC Strenghts and Weaknesses
+var npcStrongAbilities = ['Strength--powerful brawny, strong as an ox', 'Dexterity--lithe, agile, graceful', 'Constitiution--hardy, hale, healthy', 'Intelligence--studious, learned, inquisitive', 'Wisdom--perceptive, spiritual, insightful', 'Charisma--persuasive, forceful, born leader'];
+
+var npcWeakAbilities = ['Strength--feeble, scrawny', 'Dexterity--clumsy, fumbling', 'Constitiution--sickly, pale', 'Intelligence--dim-witted, slow', 'Wisdom--oblivious, abstentminded', 'Charisma--dull, boring'];
+//NPC Personality
+var npcTalents = ['Plays a musical instrument', 'Speaks several languages fluently', 'Unbelieveably lucky', 'Perfect memory', 'Great with animals', 'Great with children', 'Great at solving puzzles', 'Great at impersonations', 'Draws beautifully', 'Paints beautifully', 'Sings beautifully', 'Drinks everyone under the table', 'Expert carpenter', 'Expert cook', 'Expert dart thrower and rock skipper', 'Expert Juggler', 'Skilled actor and master of disguise', 'skilled dancer', 'Known theive\'s cant'];
+
+var npcMannerisms = ['Prone to singing, whistling, or humming quietly', 'Speaks in rhyme or some other peculiar way.', 'Particularly low or high voice', 'Slurs words, lisps, or stutters', 'Enunciates overly clearly', 'Speaks loudly', 'Whispers', 'Uses flowery speech or long words', 'Frequently uses the wrong word', 'Makes constant jokes or puns', 'Prone to predictions of doom', 'Fidgets', 'Squints', 'Stares into the distance', 'Chews something', 'Paces', 'Taps fingers', 'Bites fingernails', 'Twirls hair or tugs beard.'];
+
+var npcInteractionTraits = ['Argumentative', 'Arrogant', 'Blustering', 'Rude', 'Curious', 'Friendly', 'Honest', 'Hot Tempered', 'Irritable', 'Ponderous', 'Quiet', 'Suspicious'];
+
+var npcIdeals = ['Beauty(Good)', 'Community(Lawful)', 'Balance(Neutral)', 'Domination(Evil)', 'Change(Chaotic)', 'Aspiration(Other)', 'Charity(Good)', 'Fairness(Lawful)', 'Knowledge(Neutral)', 'Greed(Evil)', 'Creativity(Chaotic)', 'Discovery(Other)', 'Greater Good(Good)', 'Honor(Lawful)', 'Live and let live(Neutral)', 'Might(Evil)', 'Freedom(Chaotic)', 'Glory(Other)', 'Life(Good)', 'Logic(Lawful)', 'Moderation(Neutral)', 'Pain(Evil)', 'Independence(Chaotic)', 'Nation(Other)', 'Respect(Good)', 'Responsibility(Lawful)', 'Neutrality(Neutral)', 'Retribution(Evil)', 'No Limits(Chaotic)', 'Redemption(Other)', 'Self-sacrifice(Good)', 'Tradition(Lawful)', 'People(Neutral)', 'Slaughter(Evil)', 'Whimsy(Chaotic)', 'Self-knowledge(Other)'];
+
+var npcBonds = ['Dedicated to fufilling a personal life goal.', 'Protective of close family members.', 'Protective of colleagues or compatriots.', 'Loyal to a benefactor, patron, or employer.', 'Captivated by a romatic intrest.', 'Drawn to a special place.', 'Protective of a sentimental keepsake', 'Protective of a valuable possesion', 'Out for revenge.'];
+
+var npcFlawsAndSecrets = ['Forbidden love or susceptability to romance.', 'Enjoys decadent pleasures.', 'Arrogance', 'Envies another creatur\'s possessions or station.', 'Overpowering greed.', 'Prone to rage', 'Has a powerful enemy.', 'Specific phobia.', 'Shameful or scandalous history.', 'Secret crime or misdeed.', 'Possession of forbidden lore.', 'Foolhardy bravery.'];
+
+//Random Stat opperators
+var myNum1 = npcNum1[Math.floor(Math.random() * npcNum1.length)];
+var myNum2 = npcNum2[Math.floor(Math.random() * npcNum2.length)];
+var myNum3 = npcNum3[Math.floor(Math.random() * npcNum3.length)];
+var myNum4 = npcNum4[Math.floor(Math.random() * npcNum4.length)];
+var myNum5 = npcNum5[Math.floor(Math.random() * npcNum5.length)];
+var myNum6 = npcNum6[Math.floor(Math.random() * npcNum6.length)];
+
+//Male Dwarf Constructor
+var myMDName = dwarfNameMale[Math.floor(Math.random() * dwarfNameMale.length)];
+var myDClanName = dwarfClanName[Math.floor(Math.random() * dwarfClanName.length)];
+var myNpcAppearance = npcAppearance[Math.floor(Math.random() * npcAppearance.length)];
+var myNpcSA = npcStrongAbilities[Math.floor(Math.random() * npcStrongAbilities.length)];
+var myNpcWA = npcWeakAbilities[Math.floor(Math.random() * npcWeakAbilities.length)];
+var myNpcT = npcTalents[Math.floor(Math.random() * npcTalents.length)];
+var myNpcM = npcMannerisms[Math.floor(Math.random() * npcMannerisms.length)];
+var myNpcIT = npcInteractionTraits[Math.floor(Math.random() * npcInteractionTraits.length)];
+var myNpcI = npcIdeals[Math.floor(Math.random() * npcIdeals.length)];
+var myNpcB = npcBonds[Math.floor(Math.random() * npcBonds.length)];
+var myNpcFS = npcFlawsAndSecrets[Math.floor(Math.random() * npcFlawsAndSecrets.length)];
+
+//Female Dwarf Constructor
+var myFDName = dwarfNameFemale[Math.floor(Math.random() * dwarfNameFemale.length)];
+var myDClanName = dwarfClanName[Math.floor(Math.random() * dwarfClanName.length)];
+var myNpcAppearance = npcAppearance[Math.floor(Math.random() * npcAppearance.length)];
+var myNpcSA = npcStrongAbilities[Math.floor(Math.random() * npcStrongAbilities.length)];
+var myNpcWA = npcWeakAbilities[Math.floor(Math.random() * npcWeakAbilities.length)];
+var myNpcT = npcTalents[Math.floor(Math.random() * npcTalents.length)];
+var myNpcM = npcMannerisms[Math.floor(Math.random() * npcMannerisms.length)];
+var myNpcIT = npcInteractionTraits[Math.floor(Math.random() * npcInteractionTraits.length)];
+var myNpcI = npcIdeals[Math.floor(Math.random() * npcIdeals.length)];
+var myNpcB = npcBonds[Math.floor(Math.random() * npcBonds.length)];
+var myNpcFS = npcFlawsAndSecrets[Math.floor(Math.random() * npcFlawsAndSecrets.length)];
+
+//Male Elf Constructor
+var myMEName = elfMaleNames[Math.floor(Math.random() * elfMaleNames.length)];
+var myEFN = elfFamilyNames[Math.floor(Math.random() * elfFamilyNames.length)];
+var myNpcAppearance = npcAppearance[Math.floor(Math.random() * npcAppearance.length)];
+var myNpcSA = npcStrongAbilities[Math.floor(Math.random() * npcStrongAbilities.length)];
+var myNpcWA = npcWeakAbilities[Math.floor(Math.random() * npcWeakAbilities.length)];
+var myNpcT = npcTalents[Math.floor(Math.random() * npcTalents.length)];
+var myNpcM = npcMannerisms[Math.floor(Math.random() * npcMannerisms.length)];
+var myNpcIT = npcInteractionTraits[Math.floor(Math.random() * npcInteractionTraits.length)];
+var myNpcI = npcIdeals[Math.floor(Math.random() * npcIdeals.length)];
+var myNpcB = npcBonds[Math.floor(Math.random() * npcBonds.length)];
+var myNpcFS = npcFlawsAndSecrets[Math.floor(Math.random() * npcFlawsAndSecrets.length)];
+
+//Female Elf Constructor
+var myFEName = elfFemaleNames[Math.floor(Math.random() * elfFemaleNames.length)];
+var myEFN = elfFamilyNames[Math.floor(Math.random() * elfFamilyNames.length)];
+var myNpcAppearance = npcAppearance[Math.floor(Math.random() * npcAppearance.length)];
+var myNpcSA = npcStrongAbilities[Math.floor(Math.random() * npcStrongAbilities.length)];
+var myNpcWA = npcWeakAbilities[Math.floor(Math.random() * npcWeakAbilities.length)];
+var myNpcT = npcTalents[Math.floor(Math.random() * npcTalents.length)];
+var myNpcM = npcMannerisms[Math.floor(Math.random() * npcMannerisms.length)];
+var myNpcIT = npcInteractionTraits[Math.floor(Math.random() * npcInteractionTraits.length)];
+var myNpcI = npcIdeals[Math.floor(Math.random() * npcIdeals.length)];
+var myNpcB = npcBonds[Math.floor(Math.random() * npcBonds.length)];
+var myNpcFS = npcFlawsAndSecrets[Math.floor(Math.random() * npcFlawsAndSecrets.length)];
+
+//Male Halfling Constructor
+var myHMName = halflingMaleNames[Math.floor(Math.random() * halflingMaleNames.length)];
+var myHFName = halflingFamilyNames[Math.floor(Math.random() * halflingFamilyNames.length)];
+var myNpcAppearance = npcAppearance[Math.floor(Math.random() * npcAppearance.length)];
+var myNpcSA = npcStrongAbilities[Math.floor(Math.random() * npcStrongAbilities.length)];
+var myNpcWA = npcWeakAbilities[Math.floor(Math.random() * npcWeakAbilities.length)];
+var myNpcT = npcTalents[Math.floor(Math.random() * npcTalents.length)];
+var myNpcM = npcMannerisms[Math.floor(Math.random() * npcMannerisms.length)];
+var myNpcIT = npcInteractionTraits[Math.floor(Math.random() * npcInteractionTraits.length)];
+var myNpcI = npcIdeals[Math.floor(Math.random() * npcIdeals.length)];
+var myNpcB = npcBonds[Math.floor(Math.random() * npcBonds.length)];
+var myNpcFS = npcFlawsAndSecrets[Math.floor(Math.random() * npcFlawsAndSecrets.length)];
+
+//Female Halfling Constructor
+var myHFEName = halflingFemaleName[Math.floor(Math.random() * halflingFemaleName)];
+var myHFName = halflingFamilyNames[Math.floor(Math.random() * halflingFamilyNames.length)];
+var myNpcAppearance = npcAppearance[Math.floor(Math.random() * npcAppearance.length)];
+var myNpcSA = npcStrongAbilities[Math.floor(Math.random() * npcStrongAbilities.length)];
+var myNpcWA = npcWeakAbilities[Math.floor(Math.random() * npcWeakAbilities.length)];
+var myNpcT = npcTalents[Math.floor(Math.random() * npcTalents.length)];
+var myNpcM = npcMannerisms[Math.floor(Math.random() * npcMannerisms.length)];
+var myNpcIT = npcInteractionTraits[Math.floor(Math.random() * npcInteractionTraits.length)];
+var myNpcI = npcIdeals[Math.floor(Math.random() * npcIdeals.length)];
+var myNpcB = npcBonds[Math.floor(Math.random() * npcBonds.length)];
+var myNpcFS = npcFlawsAndSecrets[Math.floor(Math.random() * npcFlawsAndSecrets.length)];
+
+
+//file open functions
+function maleDwarf() {
+    window.open('file:///home/chronos/u-7bd9acf60573c8c3097a7ccd3eb7ddb47f346a65/MyFiles/Downloads/Code/NPC%20Character%20Gen/Male_Dwarf.html');
+}
+
+function femaleDwarf() {
+    window.open('file:///home/chronos/u-7bd9acf60573c8c3097a7ccd3eb7ddb47f346a65/MyFiles/Downloads/Code/NPC%20Character%20Gen/Female_Dwarf.html');
+}
+
+function maleElf() {
+    window.open('file:///home/chronos/u-7bd9acf60573c8c3097a7ccd3eb7ddb47f346a65/MyFiles/Downloads/Code/NPC%20Character%20Gen/Male_Elf.html');
+}
+
+function femaleElf() {
+    window.open('file:///home/chronos/u-7bd9acf60573c8c3097a7ccd3eb7ddb47f346a65/MyFiles/Downloads/Code/NPC%20Character%20Gen/Female_Elf.html');
+}
+
+function maleHalfling() {
+    ('file:///home/chronos/u-7bd9acf60573c8c3097a7ccd3eb7ddb47f346a65/MyFiles/Downloads/Code/NPC%20Character%20Gen/Halfling_Male.html');
+}
